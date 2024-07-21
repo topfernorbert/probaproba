eedbed0000169b319e76059c4466681a87266aa5
topfernorbert <norbert.topfer@gmail.com>
2024-07-21 18:13:09 +0200

    proba5

--- weekly_homework_6/Configs/config.py
+++ weekly_homework_6/Configs/config.py
@@ -5,7 +5,7 @@ def get_preconfigured_chrome_driver() -> webdriver.Chrome:
     options = Options()
     options.add_experimental_option('detach', True)
     options.add_argument('--headless')
-    options.add_argument('window-position=2000,50')
+    options.add_argument('window-size=1024,800')
     # options.add_argument("--lang=hu")
     options.add_argument("--lang=en")
     options.add_experimental_option('excludeSwitches', ['enable-logging'])  # Allure certificate error esetén
