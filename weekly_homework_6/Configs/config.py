770dda4c42936c93ccb283904efa538e3cc6eb24
topfernorbert <norbert.topfer@gmail.com>
2024-07-21 18:20:03 +0200

    proba5

--- weekly_homework_6/Configs/config.py
+++ weekly_homework_6/Configs/config.py
@@ -4,10 +4,9 @@ from selenium.webdriver.chrome.options import Options
 def get_preconfigured_chrome_driver() -> webdriver.Chrome:
     options = Options()
     options.add_experimental_option('detach', True)
-    options.add_argument('--headless')
-    options.add_argument('window-size=1024,800')
+    options.add_argument('window-position=2000,50')
     # options.add_argument("--lang=hu")
     options.add_argument("--lang=en")
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])  # Allure certificate error esetén
+    #options.add_experimental_option('excludeSwitches', ['enable-logging'])  # Allure certificate error esetén
 
     return webdriver.Chrome(options=options)
